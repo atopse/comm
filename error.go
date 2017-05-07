4719e15f25276a28af161d4c1568398fccaf06ce
ysqi <devysq@gmail.com>
2017-05-07 12:45:22 +0800

    Err定义优化

--- error.go
+++ error.go
@@ -11,10 +11,17 @@ type InvalidDataError struct {
 }
 
 func (e InvalidDataError) Error() string {
+	if e.data == "" {
+		return e.msg
+	}
 	return fmt.Sprintf("(%s)%s", e.data, e.msg)
 }
 
 // ErrDataIsEmpty 数据不能为空错误
-func ErrDataIsEmpty(data string) InvalidDataError {
-	return InvalidDataError{data: data, msg: "数据不能为空"}
+func ErrDataIsEmpty(data ...string) InvalidDataError {
+	err := InvalidDataError{msg: "不能为空"}
+	if len(data) > 0 {
+		err.data = data[0]
+	}
+	return err
 }
